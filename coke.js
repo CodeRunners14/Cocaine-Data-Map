1bff13611b6645a64f72c53333a14c94120b2ec1
imbenclifford <imbenclifford@gmail.com>
2014-10-16 14:55:20 +0000

    added-useless-buttons

--- coke.js
+++ coke.js
@@ -2,31 +2,39 @@ function toObject(arr) {
   var obj = {};
   for (var i = 0; i < arr.length; i++)
     obj[arr[i]["Alpha-2"]] = {
-      Alpha2: arr[i]["Alpha-2"],
-      Country: arr[i]["Country name"],
-      Retail: arr[i]["Retail price"],
-      Wholesale: arr[i]["Wholesale price"]
+      alpha2: arr[i]["Alpha-2"],
+      country: arr[i]["Country name"],
+      retail: arr[i]["Retail price"],
+      wholesale: arr[i]["Wholesale price"]
     };
   return obj;
 }
 
 var coke;
 var world;
+var selector = "retail";
 
 document.addEventListener("DOMContentLoaded", function() {
   
-      var color = d3.scale.linear()
-                    .domain([0, 1000])
+    document.getElementById("retail-btn").addEventListener("click", function(){
+      selector = "retail";
+});
+    document.getElementById("wholesale-btn").addEventListener("click", function(){
+      selector = "wholesale";
+});
+  
+    var color = d3.scale.linear()
+                    .domain([0, 1133])
                     .range(["grey", "red"]);
   
-      var svg = d3.select("#map").append("svg").attr("viewBox", "0 0 1000 500");
+    var svg = d3.select("#map").append("svg").attr("viewBox", "0 0 1000 500");
             var projection = d3.geo.robinson().translate([500, 250]);
             var path = d3.geo.path().projection(projection);
     
     function countryFill(id){
-      console.log(coke);
-      console.log(coke.id);
-      return "red";
+      if(coke[id]){
+        return color(coke[id].retail*5);
+      }      
     }
   
     function drawWorld(){
@@ -39,13 +47,8 @@ document.addEventListener("DOMContentLoaded", function() {
                .attr("id", countryID)
                .attr("d",path(countries[i]));
         }
-      
-    /* svg.selectAll("country")
-       .data(cokeObj)
-       .enter()
-         .attr("fill", function(d) {return color(d.idNAME["Wholesale price"]);});*/
-
      }
+  
      d3.json("world.topo.json", function (worldy) {
         world = worldy;
         if (coke) {
